6124deb4c091d83e6d03272d27904f884c16e9e5
LivinkumarStackly <livinkumar@thestackly.com>
2025-11-13 10:55:23 +0530

    Introduction

---
+++ 1)Introduction.js
@@ -0,0 +1,29 @@
+// alert("Vanakkam da mapla...!")
+
+// vra a=10
+// var a=14
+// a=2
+
+
+// let a=10
+// let a=100
+// a=100
+
+// const a=10
+// a=100
+
+// let a=null;
+
+// console.log(a);
+
+
+
+// console.log(typeof true);
+
+// let x="761"
+// console.log(x);
+// console.log(Number(x));
+
+console.log(Number("hello"));
+
+
