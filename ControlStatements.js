63058cc7233ff2f5c28cfad1e1e3ae5d08e43aa0
LivinkumarStackly <livinkumar@thestackly.com>
2025-12-04 18:21:28 +0530

    function

---
+++ ControlStatements.js
@@ -0,0 +1,139 @@
+// let age = Number(prompt("Enter Your Age: "))
+
+// if (age >= 18) console.log("You can vote");
+// else console.log("You cannot vote");
+
+
+// console.log("123");
+// console.log(123);
+
+
+// while (i>5) {
+//     console.log("Step1");
+//     console.log("Step2");
+//     console.log("Step3");
+//     console.log("-----------------");
+//     i--
+// }
+
+// for (let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+// let i=0
+// do {
+//     console.log("Step1");
+//     console.log("Step2");
+//     console.log("Step3");
+//     console.log("-----------------");
+//     i--
+// } while (i>5)
+
+// console.log("Loop ends!");
+
+// no of executions: 1
+// i : 0,-1
+
+
+// for (let i = 0; i <= 50; i++) {
+//     console.log("-----------------");
+//     console.log(i);
+//     console.log("-----------------");
+//     if (i == 25) {
+//         break
+//     }
+// }
+
+// console.log("Loop ends!");
+
+
+// 1,2,4,7,8,11,13,14,"buzz",16,17,19...98
+
+// %3==> skip
+// %5==> skip
+// both ==> "buzz"
+
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log("Buzz");
+
+//     } else if (i % 3 == 0 || i % 5 == 0) {
+//         continue
+//     }
+//     else {
+//         console.log(i);   
+//     }
+// }
+
+// for(let i=1;i<=5;i++){
+
+//     for(let j=1;j<=5;j++){
+
+//         for(let k=1;k<=5;k++){
+//             console.log("Sollay sunni!");
+//         }
+//     }
+
+// }
+
+// console.log("Done!");
+
+
+// secretNum = 10
+
+// while (true) {
+//     let guessNum = Number(prompt("Enter the random Number"))
+
+//     if (guessNum == secretNum) {
+//         console.log("Your won");
+//         console.log("loop End!");
+//         break
+
+//     } else {
+//         console.log("Try Again");
+//     }
+// }
+
+//loop
+
+// let i = 0
+
+// while (i<5000) {
+//     console.log(123);
+//     i++
+// }
+
+// console.log("Loop ends");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let oolu = 0
+
+// do {
+//     console.log("Rajjjjj");
+//     oolu++
+// } while (oolu > 100) 
+
+// while (oolu > 100) {
+//     console.log("Rajjjjj");
+//     oolu++
+// }
+
+// for(let bat=0; bat<100; bat++){
+//     console.log("Meiyazhan");
+// }
+
---
+++ Functions.js
@@ -0,0 +1,81 @@
+// raj()
+
+
+// function raj() {
+//     console.log("Step1");
+//     console.log("Step2");
+//     console.log("Step3");
+//     console.log("Over");
+// }
+
+// const raj = () => {
+//     console.log("Step1");
+//     console.log("Step2");
+//     console.log("Step3");
+//     console.log("Over");
+// }
+
+// raj()
+// raj()
+// raj()
+
+// console.log(1234);
+
+
+// raj()
+// raj()
+
+
+// const raj = (a, b) => {
+//     console.log(a + b);
+
+// }
+
+// raj(6, 7)
+// raj(6, 70)
+// raj(67, 76)
+
+
+
+
+// const raj = (a, b) => {
+//     if (a > b) {
+//         console.log(a);
+//     } else {
+//         console.log(b);
+//     }
+// }
+
+// const raj =(a,b) =>{
+//     console.log(a+b);
+
+// }
+
+// raj(55,45)
+// raj(77,78)
+// raj(-55,-65)
+
+// raj("Dups","Puffs")/
+
+
+function leo(a, b, c) {
+    if (a + b < b + c) {
+        if (a == b || a < b) {
+            console.log(a);
+        } else {
+            console.log(b);
+
+        }
+    } else {
+        if (c == b || c < b) {
+            console.log(c);
+        } else {
+            console.log(b);
+
+        }
+    }
+}
+
+
+leo(3, 1, 11)
+
